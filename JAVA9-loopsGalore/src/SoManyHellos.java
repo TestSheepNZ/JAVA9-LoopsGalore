d216beb8f591379bedc42e8ab9e2fe902ee67bb5
TestSheepNZ <mike_talks@hotmail.com>
2016-10-06 21:11:33 +1300

    Coding standards

--- JAVA9-loopsGalore/src/SoManyHellos.java
+++ JAVA9-loopsGalore/src/SoManyHellos.java
@@ -13,7 +13,7 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class soManyHellos {
+public class SoManyHellos {
 	
     public static void multiLingualHelloWorld (int langID)
     {
